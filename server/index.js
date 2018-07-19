547dc48a6972d30e0f6a6c49b7aaed9e9b62e621
talyh <taly.hotimsky@gmail.com>
2018-07-19 00:53:48 -0400

    Get routes for list and individual celebrities

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const app = express()
 const port = 3001
+const MongoClient = require('mongodb').MongoClient
+const ObjectId = require('mongodb').ObjectID;
+const dbURL = "mongodb://localhost:27017/CelebrityHologramStore"
 
 app.listen(port, () => {
     console.log(`app is live at port ${port}`)
@@ -18,27 +21,22 @@ app.get("/", (req, res) => {
 
 app.get("/celebrities", (req, res) => {
 
-    const dummyData = [
-        {
-            "_id": "1",
-            "name": "Woody Allen",
-            "roles": ["Writer", "Director", "Actor"],
-            "pictureURL": "https://m.media-amazon.com/images/M/MV5BMTI1MjU3MTI2MF5BMl5BanBnXkFtZTcwMDgxNTE4MQ@@._V1_UY317_CR1,0,214,317_AL_.jpg",
-            "detailsURL": "https://www.imdb.com/name/nm0000095/?ref_=fn_al_nm_1"
-        },
-        {
-            "_id": "2",
-            "name": "Christopher Nolan",
-            "roles": ["Writer", "Director"],
-            "pictureURL": "https://m.media-amazon.com/images/M/MV5BNjE3NDQyOTYyMV5BMl5BanBnXkFtZTcwODcyODU2Mw@@._V1_UY317_CR7,0,214,317_AL_.jpg",
-            "detailsURL": "https://www.imdb.com/name/nm0634240/?ref_=fn_al_nm_1"
-        }
-    ]
-
-    if (req.query.id) {
-        res.send(dummyData.filter(celebrity => celebrity._id === req.query.id))
+    const callback = result => {
+        res.send(result)
     }
-    else {
-        res.send(dummyData)
-    }
+    read(req.query.id, callback)
 })
+
+const read = (id, callback) => {
+    MongoClient.connect(dbURL, { useNewUrlParser: true }, (err, client) => {
+        if (err) { throw err }
+
+        const db = client.db("CelebrityHologramStore")
+
+        db.collection("Celebrities").find(id && { "_id": ObjectId(id) }).toArray((err, result) => {
+            if (err) { throw err }
+
+            callback(result)
+        })
+    })
+}
