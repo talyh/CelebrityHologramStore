c4c1b82c4159d570e28df7f01ecbc48e884a406e
talyh <taly.hotimsky@gmail.com>
2018-07-23 23:44:31 -0400

    Sorted celebrity list

--- client/src/components/CelebrityGrid.js
+++ client/src/components/CelebrityGrid.js
@@ -11,17 +11,21 @@ const hoverScale = 1.1
 
 const CelebrityGrid = ({ celebrityList, add, close, remove, onCardHover, onCardClick }) => {
 
-    const generateCards = array => array.map(entry => <CelebrityCard
-        key={entry._id}
-        celebrity={entry}
-        callbackForClose={close}
-        callbackForRemove={remove}
-        mode={cardModes.small}
-        onHover={onCardHover}
-        onClick={onCardClick}
-        hoverScale={hoverScale}
-        cellHeight={cellSize.height}
-        rolesInPreview={2} />)
+    const generateCards = array => {
+        const sorted = [...array].sort((entry, next) => entry.name > next.name ? 1 : (next.name > entry.name) ? -1 : 0)
+        console.log(sorted)
+        return sorted.map(entry => <CelebrityCard
+            key={entry._id}
+            celebrity={entry}
+            callbackForClose={close}
+            callbackForRemove={remove}
+            mode={cardModes.small}
+            onHover={onCardHover}
+            onClick={onCardClick}
+            hoverScale={hoverScale}
+            cellHeight={cellSize.height}
+            rolesInPreview={2} />)
+    }
 
     return (
         <Mosaic id="celebrityGrid" size={cellSize} gap={gap} itemsAmount={celebrityList.length} area={window.innerWidth}>
